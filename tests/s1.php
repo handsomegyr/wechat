630a6affdcb406e77883a20d25023f0308a19cd0
handsomegyr <handsomegyr@126.com>
2018-11-16 16:32:44 +0800

    Adding pre-comit

--- tests/s1.php
+++ tests/s1.php
@@ -12,7 +12,7 @@ try {
     $openid = 'o4ELSvz-B4_DThF0Vpfrverk3IpY';
     // appid
     $appid = 'wxbf9165206b992f39'; // appID
-
+    // secret
     $secret = '08f81abfab87863a1de2cf13af417a55'; // appsecret
 
     // 获得access_token

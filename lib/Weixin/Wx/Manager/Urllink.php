606af9599d1d45209b6c3b47245571d611971c5e
handson@intonead.com <gyr@3332502!>
2022-10-31 11:46:10 +0800

    修改

--- lib/Weixin/Wx/Manager/Urllink.php
+++ lib/Weixin/Wx/Manager/Urllink.php
@@ -43,7 +43,7 @@ class Urllink
      * access_token string 是 接口调用凭证
      * path string 是 通过 URL Link 进入的小程序页面路径，必须是已经发布的小程序存在的页面，不可携带 query 。path 为空时会跳转小程序主页
      * query string 是 通过 URL Link 进入小程序时的query，最大1024个字符，只支持数字，大小写英文以及部分特殊字符：!#$&'()*+,/:;=?@-._~
-     * env_version	string	"release"	否	要打开的小程序版本。正式版为 "release"，体验版为"trial"，开发版为"develop"，仅在微信外打开时生效。
+     * env_version string "release" 否 要打开的小程序版本。正式版为 "release"，体验版为"trial"，开发版为"develop"，仅在微信外打开时生效。
      * is_expire boolean false 否 生成的 URL Link 类型，到期失效：true，永久有效：false
      * expire_type number 是 小程序 URL Link 失效类型，失效时间：0，失效间隔天数：1
      * expire_time number 是 到期失效的 URL Link 的失效时间，为 Unix 时间戳。生成的到期失效 URL Link 在该时间前有效。最长有效期为1年。expire_type 为 0 必填
@@ -117,9 +117,106 @@ class Urllink
         $params['expire_type'] = $expire_type;
         $params['expire_time'] = $expire_time;
         $params['expire_interval'] = $expire_interval;
-        $params['cloud_base'] = $cloud_base->getParams();        
+        $params['cloud_base'] = $cloud_base->getParams();
         $params['env_version'] = $env_version;
         $rst = $this->_request->post($this->_url . 'wxa/generate_urllink', $params);
+        return $this->_client->rst($rst);
+    }
+
+    /**
+     * 小程序码与小程序链接 /URL Link /查询 URL Link
+     * 查询 URL Link
+     * 调试工具
+     *
+     * 接口应在服务器端调用，详细说明参见服务端API。
+     *
+     * 本接口支持云调用。需开发者工具版本 >= 1.02.1904090（最新稳定版下载），wx-server-sdk >= 0.4.0
+     *
+     * 接口说明
+     * 接口英文名
+     * queryUrlLink
+     *
+     * 功能描述
+     * 该接口用于查询小程序 url_link 配置，及长期有效 quota
+     *
+     * 调用方式
+     * HTTPS 调用
+     *
+     * POST https://api.weixin.qq.com/wxa/query_urllink?access_token=ACCESS_TOKEN
+     *
+     * 云调用
+     * 出入参和 HTTPS 调用相同，调用方式可查看云调用说明文档
+     *
+     * 接口方法为: openapi.urllink.query
+     *
+     * 第三方调用
+     * 调用方式以及出入参和 HTTPS 相同，仅是调用的 token 不同
+     *
+     * 该接口所属的权限集 id 为：88、115
+     *
+     * 服务商获得其中之一权限集授权后，可通过使用authorizer_access_token代商家进行调用
+     *
+     * 请求参数
+     * 属性 类型 必填 说明
+     * access_token string 是 接口调用凭证，该参数为 URL 参数，非 Body 参数。使用access_token或者authorizer_access_token
+     * url_link string 否 小程序 url_link
+     * 返回参数
+     * 属性 类型 说明
+     * errcode number 错误码
+     * errmsg string 错误信息
+     * url_link_info object url_link 配置
+     * 属性 类型 说明
+     * appid string 小程序 appid
+     * path string 小程序页面路径
+     * query string 小程序页面query
+     * create_time number 创建时间，为 Unix 时间戳
+     * expire_time number 到期失效时间，为 Unix 时间戳，0 表示永久生效
+     * env_version string 要打开的小程序版本。正式版为"release"，体验版为"trial"，开发版为"develop"
+     * url_link_quota object quota 配置
+     * 属性 类型 说明
+     * long_time_used number 长期有效 url_link 已生成次数
+     * long_time_limit number 长期有效 url_link 生成次数上限
+     * visit_openid string
+     * 调用示例
+     * 示例说明: HTTPS调用示例
+     *
+     * 请求数据示例
+     *
+     * {
+     * "url_link": "https://wxaurl.cn/BQZRrcFCPvg"
+     * }
+     *
+     * 返回数据示例
+     *
+     * {
+     * "errcode": 0,
+     * "errmsg": "ok",
+     * "url_link_info": {
+     * "appid": "appid",
+     * "path": "",
+     * "query": "",
+     * "create_time": 611928113,
+     * "expire_time": 0,
+     * "env_version": "release",
+     * "cloud_base" : {
+     * "env": "",
+     * "doamin": "",
+     * "path": "",
+     * "query": "",
+     * "resource_appid": ""
+     * }
+     * },
+     * "url_link_quota": {
+     * "long_time_used": 100,
+     * "long_time_limit": 100000
+     * }
+     * }
+     */
+    public function query($url_link)
+    {
+        $params = array();
+        $params['url_link'] = $url_link;
+        $rst = $this->_request->post($this->_url . 'wxa/query_urllink', $params);
         return $this->_client->rst($rst);
     }
 }
--- lib/Weixin/Wx/Manager/Urlscheme.php
+++ lib/Weixin/Wx/Manager/Urlscheme.php
@@ -44,9 +44,9 @@ class Urlscheme
      * access_token string 是 接口调用凭证
      * jump_wxa Object 否 跳转到的目标小程序信息。
      * is_expire boolean false 否 生成的 scheme 码类型，到期失效：true，永久有效：false。
-     * expire_type	number	0 否 到期失效的 scheme 码失效类型，失效时间：0，失效间隔天数：1
+     * expire_type number 0 否 到期失效的 scheme 码失效类型，失效时间：0，失效间隔天数：1
      * expire_time number 否 到期失效的 scheme 码的失效时间，为 Unix 时间戳。生成的到期失效 scheme 码在该时间前有效。最长有效期为1年。生成到期失效的scheme时必填。
-     * expire_interval	number	否	到期失效的 scheme 码的失效间隔天数。生成的到期失效 scheme 码在该间隔时间到达前有效。最长间隔天数为365天。is_expire 为 true 且 expire_type 为 1 时必填
+     * expire_interval number 否 到期失效的 scheme 码的失效间隔天数。生成的到期失效 scheme 码在该间隔时间到达前有效。最长间隔天数为365天。is_expire 为 true 且 expire_type 为 1 时必填
      * jump_wxa 的结构
      *
      * 属性 类型 默认值 必填 说明
@@ -107,6 +107,94 @@ class Urlscheme
         $params['expire_time'] = $expire_time;
         $params['expire_interval'] = $expire_interval;
         $rst = $this->_request->post($this->_url . 'wxa/generatescheme', $params);
+        return $this->_client->rst($rst);
+    }
+
+    /**
+     * 查询 scheme 码
+     * 调试工具
+     *
+     * 接口应在服务器端调用，详细说明参见服务端API。
+     *
+     * 本接口支持云调用。需开发者工具版本 >= 1.02.1904090（最新稳定版下载），wx-server-sdk >= 0.4.0
+     *
+     * 接口说明
+     * 接口英文名
+     * queryScheme
+     *
+     * 功能描述
+     * 该接口用于查询小程序 scheme 码，及长期有效 quota。
+     *
+     * 调用方式
+     * HTTPS 调用
+     *
+     * POST https://api.weixin.qq.com/wxa/queryscheme?access_token=ACCESS_TOKEN
+     *
+     * 云调用
+     * 出入参和 HTTPS 调用相同，调用方式可查看云调用说明文档
+     *
+     * 接口方法为: openapi.urlscheme.query
+     *
+     * 第三方调用
+     * 调用方式以及出入参和 HTTPS 相同，仅是调用的 token 不同
+     *
+     * 该接口所属的权限集 id 为：88、115
+     *
+     * 服务商获得其中之一权限集授权后，可通过使用authorizer_access_token代商家进行调用
+     *
+     * 请求参数
+     * 属性 类型 必填 说明
+     * access_token string 是 接口调用凭证，该参数为 URL 参数，非 Body 参数。使用access_token或者authorizer_access_token
+     * scheme string 是 小程序 scheme 码
+     * 返回参数
+     * 属性 类型 说明
+     * errcode number 错误码
+     * errmsg string 错误信息
+     * scheme_info object scheme 配置
+     * 属性 类型 说明
+     * appid string 小程序 appid
+     * path string 小程序页面路径
+     * query string 小程序页面query
+     * create_time number 创建时间，为 Unix 时间戳
+     * expire_time number 到期失效时间，为 Unix 时间戳，0 表示永久生效
+     * env_version string 要打开的小程序版本。正式版为"release"，体验版为"trial"，开发版为"develop"
+     * scheme_quota object quota 配置
+     * 属性 类型 说明
+     * long_time_used number 长期有效 scheme 已生成次数
+     * long_time_limit number 长期有效 scheme 生成次数上限
+     * 调用示例
+     * 示例说明: HTTPS请求示例
+     *
+     * 请求数据示例
+     *
+     * {
+     * "scheme": "weixin://dl/business/?t=XTSkBZlzqmn"
+     * }
+     *
+     * 返回数据示例
+     *
+     * {
+     * "errcode": 0,
+     * "errmsg": "ok",
+     * "scheme_info": {
+     * "appid": "appid",
+     * "path": "",
+     * "query": "",
+     * "create_time": 611928113,
+     * "expire_time": 0,
+     * "env_version": "release"
+     * },
+     * "scheme_quota": {
+     * "long_time_used": 100,
+     * "long_time_limit": 100000
+     * }
+     * }
+     */
+    public function query($scheme)
+    {
+        $params = array();
+        $params['scheme'] = $scheme;
+        $rst = $this->_request->post($this->_url . 'wxa/queryscheme', $params);
         return $this->_client->rst($rst);
     }
 }
